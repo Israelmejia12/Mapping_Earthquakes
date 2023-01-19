1f56bc71161ba90a564f56254e296601289738bf
Israel Mejia <113860272+Israelmejia12@users.noreply.github.com>
2023-01-19 16:08:13 -0500

    Updating file jan 19 4:08

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -1,16 +1,36 @@
 
-
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
   accessToken: API_KEY
 
 });
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
 
@@ -40,30 +60,24 @@ let LesterBPearsonInternationalAirport =
 };
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/israelmejia12/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/Mapping_Earthquakes/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function (data) {
   console.log(data);
+
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+
+    pointTolayer: function (feature, latlng) {
+      console.log(feature);
+      return L.marker(latlng)
+  
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "<h/2>"+
+      "<hr> Airport Name: " + feature.properties.name + "<h/r>");
+    }
+  
+  }).addTo(map);
 });
-
-
-
-// Add GeoJSON data.
-L.geoJSON(LesterBPearsonInternationalAirport, {
-
-pointTolayer: function (feature, latlng) {
-  console.log(feature);
-return L.marker(latlng)
-.bindPopup("<h2>" +feature.properties.city + "<h/2>");
-
-
-  }
-
-}).addTo(map);
-
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(LesterBPearsonInternationalAirport).addTo(map);
-
