e3a6d5018d9c609a036cea593969368de1aab530
Israel Mejia <113860272+Israelmejia12@users.noreply.github.com>
2023-01-20 09:44:58 -0500

    updating files jan 20 9:44

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -1,5 +1,5 @@
 
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox/light-v10',
@@ -34,37 +34,39 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'streets' tile layer to the map.
 light.addTo(map);
 
-// Add GeoJSON data.
-let LesterBPearsonInternationalAirport =
-{
-  "type": "FeatureCollection", "features": [{
-    "type": "Feature",
-    "properties": {
-      "id": "193",
-      "name": "Lester B. Pearson International Airport",
-      "city": "Toronto",
-      "country": "Canada",
-      "faa": "YYZ",
-      "icao": "CYYZ",
-      "alt": "569",
-      "tz-offset": "-5",
-      "dst": "A",
-      "tz": "America/Toronto"
-    },
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-79.63059997559999, 43.6772003174]
-    }
-  }
-  ]
-};
+// // Add GeoJSON data.
+// let torontoData =
+
+// {"type":"Feature",
+// "properties":{"airline":"AC",
+// "airline_id":"330",
+// "src":"YYZ",
+// "src_id":"193","dst":"AUH",
+// "dst_id":"2179","codeshare":"",
+// "stops":"0","equipment":"77W"},
+// "geometry":{"type":"LineString",
+// "coordinates":[[-79.63059997559999,43.6772003174],[54.651100158691406,24.433000564575195]]}}
+
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/Israelmejia12/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  style: myStyle,
+onEachFeature: function (feature, layer) {
+  layer.bindPopup("<h3> Airport Code: " + feature.properties.airline + "<h/3>"+
+  "<hr> Destination: " + feature.properties.dst + "<h/r>");
+}
+})
+.addTo(map);
 });
