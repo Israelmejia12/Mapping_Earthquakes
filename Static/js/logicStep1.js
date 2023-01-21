63d09b0bd5854f4db93ac838882392ce051239a5
Israel Mejia <113860272+Israelmejia12@users.noreply.github.com>
2023-01-21 00:53:01 -0500

    Updating

--- Static/js/logicStep1.js
+++ Static/js/logicStep1.js
@@ -49,14 +49,20 @@ function styleInfo(feature) {
   };
 }
 
-  // This function determines the radius of the earthquake marker based on its magnitude.
-  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
-  function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 4;
-  }
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into two separate functions
+// to calculate the color and radius.
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.properties.mag),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
 
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
@@ -70,4 +76,24 @@ L.geoJSON(data, {
         // We set the style for each circleMarker using our styleInfo function.
       style: styleInfo
       }).addTo(map);
+
+      // This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
   });
